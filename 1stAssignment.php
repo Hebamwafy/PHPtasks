aa1f6dfb62b339249e9a92c60feddd446d818fa8
hebamwafy <105353510+Hebamwafy@users.noreply.github.com>
2023-07-13 21:09:35 +0300

    Add files via upload
    1st_Assignment In PHP Course From "ELZERO WEB SCHOOL " 
    contains from 4 tasks about comment and how to use it properly
    from vid 1 : vid5
    link the Assignment:https://elzero.org/php-bootcamp-assignments-lesson-from-1-to-5/

---
+++ 1stAssignment.php
@@ -0,0 +1,45 @@
+<?php
+/*
+my name is heba 
+I'm learning PHP course
+*/?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>"My first PHP Page"</title>
+  </head>
+  <body><?php echo "My first PHP Page"; ?>
+    <div><?php echo "we love " ;?></div>
+    <div><?php echo "Elzero channel " ; ?></div>
+  </body>
+</html>
+
+
+<?php
+echo "------------Task 2:-------------:<br> ";
+echo"<br>";
+
+echo 'Prevent Me From Running Please<br>';
+print 'Prevent Me From Running Please<br>';
+?>
+<?='Prevent Me From Running Please<br>';?>
+
+<?php
+echo"<br>";
+echo "------------Task 3:-------------:<br> ";
+echo"<br>";
+/* My Application
+ Version 1.0
+ Created By Elzero
+ */
+
+ echo"<br>";
+ echo "------------Task 4:-------------:<br> ";
+echo"<br>";
+// ## First Comment           "Right comment" 
+# // # Second Comment         "Right comment" 
+/* /* /* Third Comment */    //"Right comment"
+////// Fourth Comment          "wrong comment" 
